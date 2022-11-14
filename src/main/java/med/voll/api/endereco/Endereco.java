75ecd3d41085a0f3256a34b1472f172ddea016ed
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-11-14 19:20:33 -0300

    aula 03: spring data jpa

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,31 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+    public Endereco(DadosEndereco endereco) {
+        this.logradouro = endereco.logradouro();
+        this.bairro = endereco.bairro();
+        this.cep = endereco.cep();
+        this.numero = endereco.numero();
+        this.complemento = endereco.complemento();
+        this.cidade = endereco.cidade();
+        this.uf = endereco.uf();
+    }
+}
