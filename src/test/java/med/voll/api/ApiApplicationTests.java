efd444f37dbf2418db9f8b5bc080460adda8b04a
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-11-14 17:43:00 -0300

    aula 01: criação do projeto

---
+++ src/test/java/med/voll/api/ApiApplicationTests.java
@@ -0,0 +1,17 @@
+package med.voll.api;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
